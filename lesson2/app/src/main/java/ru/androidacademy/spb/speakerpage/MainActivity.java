dcb00fc230e281d11a91fe6565c8c83f9c4827e0
daniiliv <dania-spb@mail.ru>
2019-04-10 20:04:14 +0300

    Speaker information.

--- lesson2/app/src/main/java/ru/androidacademy/spb/speakerpage/MainActivity.java
+++ lesson2/app/src/main/java/ru/androidacademy/spb/speakerpage/MainActivity.java
@@ -2,13 +2,12 @@ package ru.androidacademy.spb.speakerpage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tanay_pant_activity);
     }
 }
